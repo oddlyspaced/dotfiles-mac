bf13ba18c4ad7bf61d23c10402ef8476936cab57
oddlyspaced <oddlyspaced1@gmail.com>
2023-10-07 11:17:45 +0530

    fish: init config

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/opt/openjdk/bin
+
+starship init fish | source
