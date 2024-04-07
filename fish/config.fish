6272bfcd341bf38b038e0c575df808c41f175b13
oddlyspaced <oddlyspaced1@gmail.com>
2024-04-07 23:11:32 +0530

    fish: conda init

--- fish/config.fish
+++ fish/config.fish
@@ -3,8 +3,17 @@ if status is-interactive
 end
 
 fish_add_path /opt/homebrew/bin
-fish_add_path /opt/homebrew/opt/openjdk/bin
+fish_add_path /opt/homebrew/opt/openjdk@11/bin
+fish_add_path /opt/homebrew/anaconda3/bin
 
 starship init fish | source
 
 export EDITOR=nvim
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/homebrew/anaconda3/bin/conda
+    eval /opt/homebrew/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
